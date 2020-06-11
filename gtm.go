552f38ebd4039787a043bdeceffe88711876cefb
quanhengzhuang <quanhengzhuang@gmail.com>
2020-06-12 00:45:05 +0800

    add comments

--- gtm.go
+++ gtm.go
@@ -4,11 +4,15 @@ import (
 	"fmt"
 )
 
+// GTM is the definition of global transaction manager.
+// Including multiple normalPartners, one uncertainPartner, multiple certainPartners.
 type GTM struct {
 	normalPartners   []NormalPartner
 	uncertainPartner UncertainPartner
 	certainPartners  []CertainPartner
 }
+
+type Result int8
 
 const (
 	Success = 0
@@ -16,16 +20,27 @@ const (
 	Unknown = -1
 )
 
+// NormalPartner is a normal participant.
+// This participant needs three methods to implement 2PC.
+// In business, DoNext is often omitted and can directly return success.
+// If Do returns a failure, Undo will not be executed, because gtm thinks there is no impact.
+// If Do returns uncertainty, Undo will be executed.
 type NormalPartner interface {
 	Do() (bool, error)
 	DoNext() error
 	Undo() error
 }
 
+// UncertainPartner is an uncertain (unstable) participant.
+// The execution result only accepts success and failure, and the result will be retried if uncertain.
+// Only one participant of this type is allowed in each gtm transaction.
 type UncertainPartner interface {
 	Do() (bool, error)
 }
 
+// CertainPartner is a certain (stable) participant.
+// The execution result can only be success, other results will be retried.
+// This type of participant is placed at the end of the gtm transaction, allowing multiple.
 type CertainPartner interface {
 	DoNext() error
 }
