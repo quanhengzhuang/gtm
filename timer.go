8e2dc4553cd513c3bfa4a13163137634985a1aba
quanhengzhuang <quanhengzhuang@gmail.com>
2020-06-21 00:03:21 +0800

    fix time calc err

--- timer.go
+++ timer.go
@@ -13,7 +13,7 @@ type DoubleTimer struct {
 }
 
 func (t *DoubleTimer) CalcRetryTime(times int, minInterval time.Duration) time.Time {
-	interval := time.Duration(math.Pow(2, float64(times)))
+	interval := time.Duration(math.Pow(2, float64(times))) * time.Second
 	if interval < minInterval {
 		interval = minInterval
 	}
