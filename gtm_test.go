8fe52da7423bce79a96117887b205a2e3de85a5d
quanhengzhuang <quanhengzhuang@gmail.com>
2020-06-21 19:35:32 +0800

    use dbstorage

--- gtm_test.go
+++ gtm_test.go
@@ -1,32 +1,24 @@
 package gtm_test
 
 import (
-	"fmt"
-	"log"
-	"testing"
-	"time"
-
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 	"github.com/quanhengzhuang/gtm"
+	"log"
+	"testing"
 )
 
 func init() {
-	storage := NewLevelStorage()
-	// gob.RegisterName("*gtm.Payer", &Payer{})
-	// gob.RegisterName("*gtm.OrderCreator", &OrderCreator{})
-	storage.Register(&Payer{})
-	storage.Register(&OrderCreator{})
-
-	gtm.SetStorage(storage)
-
 	db, err := gorm.Open("mysql", "root:root1234@/gtm?charset=utf8&parseTime=True&loc=Local")
 	if err != nil {
 		log.Fatalf("db open failed: %v", err)
 	}
+	db.LogMode(true)
 
-	dbs := gtm.NewDBStorage(db)
-	gtm.SetStorage(dbs)
+	s := gtm.NewDBStorage(db)
+	s.Register(&Payer{}, &OrderCreator{})
+
+	gtm.SetStorage(s)
 }
 
 func TestNew(t *testing.T) {
@@ -39,42 +31,31 @@ func TestNew(t *testing.T) {
 		t.Logf("tx's result = success")
 	case gtm.Fail:
 		t.Logf("tx's result = fail. err = %+v", err)
-	case gtm.Uncertain:
-		t.Logf("tx's result = uncertain: err = %v", err)
+	default:
+		t.Logf("tx's result = %v: err = %v", result, err)
 	}
 }
 
-func TestBackground(t *testing.T) {
-	tx := gtm.New()
-	tx.AddNormalPartners(&Payer{OrderID: "100001", UserID: 20001, Amount: 99})
-	tx.AddUncertainPartner(&OrderCreator{OrderID: "100001", UserID: 20001, ProductID: 31, Amount: 99})
+func TestAsync(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		tx := gtm.New()
+		tx.AddNormalPartners(&Payer{OrderID: "100001", UserID: 20001, Amount: 99})
+		tx.AddUncertainPartner(&OrderCreator{OrderID: "100001", UserID: 20001, ProductID: 31, Amount: 99})
 
-	if err := tx.ExecuteBackground(); err != nil {
-		log.Printf("execute background err = %v", err)
-	}
-}
-
-func retry() error {
-	count := 10
-
-	for {
-		transactions, results, errs, err := gtm.RetryTimeoutTransactions(count)
-		if err != nil {
-			return fmt.Errorf("retry err: %v", err)
-		}
-
-		for k, tx := range transactions {
-			log.Printf("retry id = %v, result = %v, err = %v", tx.ID, results[k], errs[k])
-		}
-
-		if len(transactions) == 0 {
-			time.Sleep(time.Minute)
+		if err := tx.ExecuteAsync(); err != nil {
+			t.Errorf("execute background err = %v", err)
 		}
 	}
 }
 
 func TestRetry(t *testing.T) {
-	if err := retry(); err != nil {
-		t.Errorf("retry failed: %v", err)
+	count := 10
+	transactions, results, errs, err := gtm.RetryTimeoutTransactions(count)
+	if err != nil {
+		t.Errorf("retry err: %v", err)
+	}
+
+	for k, tx := range transactions {
+		t.Logf("retry id = %v, result = %v, err = %v", tx.ID, results[k], errs[k])
 	}
 }
